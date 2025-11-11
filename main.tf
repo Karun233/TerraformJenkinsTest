f7d2506b4c8690a2bb0449fc058ea47499084e96
Karun233 <karunravikumar23@gmail.com>
2025-11-11 13:06:20 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.16.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "e7103323-a982-4c91-bb5f-7960f7fa8760"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "RG1"
+    storage_account_name = "<your storage account>"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "RG2"
+  location = "westeurope"
+}
